2ce5b1157f61d17131ae2d6e179d391c8fc88c3c
Leal-Matheus <theusleal04@hotmail.com>
2022-09-21 21:12:58 -0300

    feat: configura startup e criptografia

--- BackEnd/digibank-back/digibank-back/Domains/Aco.cs
+++ BackEnd/digibank-back/digibank-back/Domains/Aco.cs
@@ -7,6 +7,7 @@ namespace digibank_back.Domains
 {
     public partial class Aco
     {
+        
         public short IdAcao { get; set; }
         public short? IdUsuario { get; set; }
         public byte? IdAcoesOptions { get; set; }
---
+++ BackEnd/digibank-back/digibank-back/Utils/Criptografia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace digibank_back.Utils
+{
+    public class Criptografia
+    {
+        /// <summary>
+        /// Criptografa uma senha
+        /// </summary>
+        /// <param name="senha">senha que será criptografada</param>
+        /// <returns>Uma senha criptografada</returns>
+        public static string CriptografarSenha(string senha)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(senha);
+        }
+
+        /// <summary>
+        /// Compara uma as senhas recebidas
+        /// </summary>
+        /// <param name="senhaForm">Senha vinda do front</param>
+        /// <param name="senhaBanco">Senha criptografado no banco</param>
+        /// <returns>True ou False baseado no resultado da comparação</returns>
+        public static bool CompararSenha(string senhaForm, string senhaBanco)
+        {
+            return BCrypt.Net.BCrypt.Verify(senhaForm, senhaBanco);
+        }
+    }
+}
