4f48134c0c91e9758427474f58603c61e844c264
Leal-Matheus <theusleal04@hotmail.com>
2023-04-12 15:10:50 -0500

    feat: login funcionando

--- digibank/src/pages/Login/Login.tsx
+++ digibank/src/pages/Login/Login.tsx
@@ -1,12 +1,36 @@
 import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { parseJwt } from '../../services/auth';
+import RedLogo from '../../assets/img/logoVermelha.png';
 import api from '../../services/api';
+
+const CssTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: '#b3b3b3',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: '#b3b3b3',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#b3b3b3',
+    },
+    '&:hover fieldset': {
+      borderColor: '#b3b3b3',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#b3b3b3',
+    },
+  },
+});
 
 function Login() {
   const [cpf, setCpf] = useState('');
   const [senha, setSenha] = useState('');
   const navigate = useNavigate();
+  // eslint-disable-next-line no-bitwise
 
   const FazerLogin = (event: any) => {
     event.preventDefault();
@@ -31,14 +55,49 @@ function Login() {
       });
   };
   return (
-    <div>
-      <p>Login</p>
-      <form onSubmit={(event) => FazerLogin(event)}>
-        <input value={cpf} onChange={(evt) => setCpf(evt.target.value)} />
-        <input value={senha} onChange={(evt) => setSenha(evt.target.value)} />
-        <button type="submit">Entrar</button>
-      </form>
-    </div>
+    <body>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat&family=Quicksand&display=swap"
+        rel="stylesheet"
+      />
+      <header className="headerLogin">
+        <img src={RedLogo} alt="logo header vermelha" />
+      </header>
+      <main className="mainLogin container">
+        <div className="bannerLogin">
+          <img src={RedLogo} alt="logo header vermelha" />
+        </div>
+        <div className="formArea">
+          <h1>login</h1>
+          <p className="textLogin">acesse sua conta e tenha acesso a tudo sobre seu cartão</p>
+          <form className="formLogin" onSubmit={(event) => FazerLogin(event)}>
+            <div className="double-input">
+              <CssTextField
+                id="outlined-basic"
+                label="CPF"
+                variant="outlined"
+                fullWidth
+                // className={classes.textField}
+                value={cpf}
+                onChange={(evt) => setCpf(evt.target.value)}
+              />
+              <CssTextField
+                id="outlined-basic"
+                label="Senha"
+                variant="outlined"
+                fullWidth
+                // className="inputLogin"
+                value={senha}
+                onChange={(evt) => setSenha(evt.target.value)}
+              />
+            </div>
+            <button className="btnLogin" type="submit">
+              Entrar
+            </button>
+          </form>
+        </div>
+      </main>
+    </body>
   );
 }
 
--- digibank/src/services/api.ts
+++ digibank/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/',
+  baseURL: 'https://858d-2804-14c-c1-94e0-ed84-1eaa-5b97-f59b.ngrok-free.app/api/',
 });
 
 export default api;
